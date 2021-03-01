be1c7c2ffe7850a223e007cc950ab8ea95073529
JoshuaSeth <SB@accette.eu>
2021-03-01 16:26:26 +0100

    start on model 2

---
+++ LSTM_Model_2/QuoteGenerator2.py
@@ -0,0 +1,74 @@
+# 1. Import the libraries
+# keras module for building LSTM 
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Embedding, LSTM, Dense, Dropout
+from keras.preprocessing.text import Tokenizer
+from keras.callbacks import EarlyStopping
+from keras.models import Sequential
+import keras.utils as ku 
+
+# set seeds for reproducability
+from tensorflow import set_random_seed
+from numpy.random import seed
+set_random_seed(2)
+seed(1)
+
+import pandas as pd
+import numpy as np
+import string, os 
+
+import warnings
+warnings.filterwarnings("ignore")
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+#2. Load the dataset
+curr_dir = '../input/'
+all_headlines = []
+for filename in os.listdir(curr_dir):
+    if 'Articles' in filename:
+        article_df = pd.read_csv(curr_dir + filename)
+        all_headlines.extend(list(article_df.headline.values))
+        break
+
+all_headlines = [h for h in all_headlines if h != "Unknown"]
+len(all_headlines)
+
+
+def clean_text(txt):
+    txt = "".join(v for v in txt if v not in string.punctuation).lower()
+    txt = txt.encode("utf8").decode("ascii",'ignore')
+    return txt 
+
+corpus = [clean_text(x) for x in all_headlines]
+corpus[:10]
+
+#3. Dataset preparation
+#3.1 Cleaning
+def clean_text(txt):
+    txt = "".join(v for v in txt if v not in string.punctuation).lower()
+    txt = txt.encode("utf8").decode("ascii",'ignore')
+    return txt 
+
+corpus = [clean_text(x) for x in all_headlines]
+corpus[:10]
+
+#3.2 Generating sequences of N-Gram tokens
+tokenizer = Tokenizer()
+
+def get_sequence_of_tokens(corpus):
+    ## tokenization
+    tokenizer.fit_on_texts(corpus)
+    total_words = len(tokenizer.word_index) + 1
+    
+    ## convert data to sequence of tokens 
+    input_sequences = []
+    for line in corpus:
+        token_list = tokenizer.texts_to_sequences([line])[0]
+        for i in range(1, len(token_list)):
+            n_gram_sequence = token_list[:i+1]
+            input_sequences.append(n_gram_sequence)
+    return input_sequences, total_words
+
+inp_sequences, total_words = get_sequence_of_tokens(corpus)
+inp_sequences[:10]
+
--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # MachineLearningVu
 
-## [Model 1](LSTM_Model_1/LSTMQuoteGeneration.py)
+## [Model 1](LSTM_Model_1/QuoteGenerator1.py)
 Tutorial: [Tensorflow Text Generation](https://www.tensorflow.org/tutorials/text/text_generation)
+
+**Language Representation:** Tokenisation of characters
+
+**Quotes Representation:** Concatination into large continious text
 
 **Description:** This model currently uses all quotes as if it would be one big continuous text. Of course the quotes are separate contextual self-contained entities and not part of a continuous text. I.e. one quote has nothing to do with the next one. Currently not shuffling the quotes as a temporary solution to mitigate this. When all quotes about age are together and not shuffled at least the context in the big text is somewhat more appropriate. Have to link into how to provide data as separate self-contained text snippets. Will submit trained model after finished training.
 
 **Notes:** Costumized training section of this tutorial is not incorporated.
 
 **Current ETA:** 1 hour (2019 MacBook i7 CPU)
+
+
+# MachineLearningVu
+
+## [Model 2](LSTM_Model_2/QuoteGenerator2.py)
+Tutorial: [Kaggle Beginners Text Generation](https://www.kaggle.com/shivamb/beginners-guide-to-text-generation-using-lstms)
+
+**Language Representation:** Tokenisation of words
+
+**Quotes Representation:** -
+
+**Description:** Unfinished work in progress
+
+**Notes:** -
+
+**Current ETA:** -
